09cf5fe5a87ea740881ffd82c9d48d7612cf2590
Russell Gayle <russ.gayle@gmail.com>
2014-11-03 12:03:29 -0600

    update test_sdl to do a little more

--- demos/test_sdl/test_sdl.cpp
+++ demos/test_sdl/test_sdl.cpp
@@ -9,6 +9,21 @@ int main(int argc, char* argv[]) {
     return 1;
   }
 
+  SDL_Window *window = SDL_CreateWindow("Hi from SDL", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
+  if (window == NULL) {
+  	std::cerr << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
+  	SDL_Quit();
+  	return 1;
+  }
+
+  SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+  if (renderer == NULL) {
+  	std::cerr << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
+  	SDL_DestroyWindow(window);
+  	SDL_Quit();
+  	return 1;
+  }
+
   SDL_Quit();
   return 0;
 }
