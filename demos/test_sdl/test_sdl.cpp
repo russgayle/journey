e9d1df014d52d7cc4c1e42a2537679b878dba6a4
Russell Gayle <russ.gayle@gmail.com>
2014-11-11 00:13:51 -0600

    Make the demo program do a little more.

--- demos/test_sdl/test_sdl.cpp
+++ demos/test_sdl/test_sdl.cpp
@@ -9,21 +9,22 @@ int main(int argc, char* argv[]) {
     return 1;
   }
 
-  SDL_Window *window = SDL_CreateWindow("Hi from SDL", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
+  SDL_Window *window = SDL_CreateWindow("Very Simple SDL", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
   if (window == NULL) {
   	std::cerr << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
   	SDL_Quit();
   	return 1;
   }
 
-  SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-  if (renderer == NULL) {
-  	std::cerr << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
-  	SDL_DestroyWindow(window);
-  	SDL_Quit();
-  	return 1;
-  }
+  SDL_Surface *surface = SDL_GetWindowSurface(window);
 
+  SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, 0xFF, 0xFF, 0xFF));
+
+  SDL_UpdateWindowSurface(window);
+
+  SDL_Delay(2000);
+
+  SDL_DestroyWindow(window);
   SDL_Quit();
   return 0;
 }
